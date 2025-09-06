b5145173d01e2d5600033638ac9a106367363d7a
zeta <ankitgahlyan44@gmail.com>
2025-09-06 11:47:12 +0530

    changed name of "add to screen" icon, + basicSSl plugin + docs as default buildDir

--- src/App.tsx
+++ src/App.tsx
@@ -33,47 +33,7 @@
 // }
 
 // // export default App
-// import './App.css';
-// import { TonConnectButton } from '@tonconnect/ui-react';
-
-// function App() {
-//   return (
-//     <div>
-//       <TonConnectButton />
-//     </div>
-//   );
-// }
-
-// export default App
-
-// import './App.css';
-// import { TonConnectButton } from '@tonconnect/ui-react';
-// import { useCounterContract } from './hooks/useCounterContract';
-
-// function App() {
-//   const { value, address } = useCounterContract();
-
-//   return (
-//     <div className='App'>
-//       <div className='Container'>
-//         <TonConnectButton />
-
-//         <div className='Card'>
-//           <b>Counter Address</b>
-//           <div className='Hint'>{address?.slice(0, 30) + '...'}</div>
-//         </div>
-
-//         <div className='Card'>
-//           <b>Counter Value</b>
-//           <div>{value ?? 'Loading...'}</div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App
-import '@twa-dev/sdk'
+//import '@twa-dev/sdk'
 import './App.css';
 import { TonConnectButton } from '@tonconnect/ui-react';
 import { useTonConnect } from './hooks/useTonConnect';
--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import WebApp from '@twa-dev/sdk';
 
+WebApp.ready();
 // this manifest is used temporarily for development purposes
 const manifestUrl = 'https://ankitgahlyan.github.io/fossfiat/tonconnect-manifest.json';
 
